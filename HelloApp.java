45ae6d2d203993773c29bd55d69cf6d70ca746b8
hrritika <hrritikagopinath18@gmail.com>
2026-03-11 09:39:29 +0530

    UC3 COMPLETED

--- HelloApp.java
+++ HelloApp.java
@@ -1,13 +1,15 @@
-public class HelloName {
+public class HelloApp {
+
     public static void main(String[] args) {
 
+        String name;
+
         if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello " + name);
-        } 
-        else {
-            System.out.println("Please provide your name as a command line argument.");
+            name = args[0];
+        } else {
+            name = "World";
         }
 
+        System.out.println("Hello " + name);
     }
 }
